4fc10ac8b9c0720eb6e7c6c9488988fd4cc99b3d
adriib38 <adrianbenitezb7@gmail.com>
2025-01-10 19:25:46 +0100

    Fix CORS

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const cookieParser = require("cookie-parser");
 
 const app = express();
 
-const allowedOrigin = process.env.CORS_ORIGINS.split(",");
+const allowedOrigin = process.env.CORS_ORIGIN;
 
 app.use(
   cors({
