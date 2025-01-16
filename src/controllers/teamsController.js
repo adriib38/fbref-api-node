81cbb001fa0ea27ad4e47e97b0a4c3ad6430c664
adriib38 <adrianbenitezb7@gmail.com>
2025-01-16 22:26:43 +0100

    Fix getTeamById

--- src/controllers/teamsController.js
+++ src/controllers/teamsController.js
@@ -5,7 +5,7 @@ const getTeamById = (req, res) => {
     if (err) {
       return res.status(500).json({ message: "Error getting team", error: err });
     }
-    if (results.length === 0) {
+    if (!results || results.length === 0) {
       return res.status(404).json({ message: "Team not found" });
     }
 
--- src/services/TeamService.js
+++ src/services/TeamService.js
@@ -1,4 +1,4 @@
-const db = require('../database'); // Tu configuración de conexión a la base de datos
+const db = require("../database"); // Tu configuración de conexión a la base de datos
 
 class Team {
   static getTeamById(teamName, callback) {
@@ -11,15 +11,15 @@ class Team {
 
     db.query(query1, [`%${teamName}%`], (err, results) => {
       if (err) return callback(err);
-      if (results.length === 0) {
-        return callback(null, []);
+      if (!results || results.length === 0) {
+        return callback(null, []); // Devuelve un arreglo vacío si no hay resultados.
       }
 
       const leagueTable = results[0].league;
 
       //2. Get league team row
-      const query2 = `SELECT *, ? as league FROM ?? WHERE squad = ?;`;
-      db.query(query2, [leagueTable, leagueTable, teamName], (err, results) => {
+      const query2 = `SELECT *, ? as league FROM ?? WHERE squad like ?;`;
+      db.query(query2, [leagueTable, leagueTable, `%${teamName}%`], (err, results) => {
         if (err) return callback(err);
 
         callback(null, results[0]);
