07e09d63b39f2f4a05c425c06e5a1a2746992f52
adriib38 <adrianbenitezb7@gmail.com>
2025-01-21 20:21:13 +0100

    Modified ssl database

--- src/database.js
+++ src/database.js
@@ -1,22 +1,20 @@
 const mysql = require('mysql2');
 const fs = require('fs');
 
-
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   port: process.env.DB_PORT,
-  // ssl: {
-  //   ca: ca,
-  //   minVersion: 'TLSv1.2',
-  //   rejectUnauthorized: true
-  // },
-  ssl: false,
+  ssl: {
+    ca: process.env.DB_SSL_CA.replace(/\\n/g, '\n'),
+    minVersion: 'TLSv1.2',
+    rejectUnauthorized: true,
+  },
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
 });
 
 pool.getConnection((err, connection) => {
