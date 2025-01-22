8ff6e71d333cafe412ee5486397af2c75cebc4f8
adriib38 <adrianbenitezb7@gmail.com>
2025-01-22 10:54:14 +0100

    Añadir cabecera para permitir credenciales en CORS

--- src/app.js
+++ src/app.js
@@ -16,6 +16,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Headers",
     "Content-Type, Authorization, X-Requested-With"
   );
+  res.header("Access-Control-Allow-Credentials", "true");
   next();
 });
 
