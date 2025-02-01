fd2fcb017a47865107b62e3c84e7cfcd63bfebaf
adriib38 <adrianbenitezb7@gmail.com>
2025-02-01 20:10:09 +0100

    Fix fecha get next games query

--- src/services/GamesService.js
+++ src/services/GamesService.js
@@ -2,7 +2,7 @@ const db = require("../database");
 
 class Game {
   static getNextGames(d, callback) {
-    let q = `SELECT * FROM ( SELECT Home, Away, Date, Score, Time, Venue, 'laliga' AS league FROM games_laliga UNION ALL SELECT Home, Away, Date, Score, Time, Venue, 'hypermotion' AS league FROM games_hypermotion UNION ALL SELECT Home, Away, Date, Score, Time, Venue, 'ligue1' AS league FROM games_ligue1 UNION ALL SELECT Home, Away, Date, Score, Time, Venue, 'seriea' AS league FROM games_seriea UNION ALL SELECT Home, Away, Date, Score, Time, Venue, 'premierleague' AS league FROM games_premierleague UNION ALL SELECT Home, Away, Date, Score, Time, Venue, 'bundesliga' AS league FROM games_bundesliga ) AS combined_games WHERE Score IS NULL AND DATE >= '2025-01-29' ORDER BY DATE LIMIT 30;`;
+    let q = `SELECT * FROM ( SELECT Home, Away, Date, Score, Time, Venue, 'laliga' AS league FROM games_laliga UNION ALL SELECT Home, Away, Date, Score, Time, Venue, 'hypermotion' AS league FROM games_hypermotion UNION ALL SELECT Home, Away, Date, Score, Time, Venue, 'ligue1' AS league FROM games_ligue1 UNION ALL SELECT Home, Away, Date, Score, Time, Venue, 'seriea' AS league FROM games_seriea UNION ALL SELECT Home, Away, Date, Score, Time, Venue, 'premierleague' AS league FROM games_premierleague UNION ALL SELECT Home, Away, Date, Score, Time, Venue, 'bundesliga' AS league FROM games_bundesliga ) AS combined_games WHERE Score IS NULL AND DATE >= ? ORDER BY DATE LIMIT 30;`;
 
     db.query(q, [d], (err, results) => {
       callback(err, results);
